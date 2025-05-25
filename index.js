767b6f95e95eccb69d827fa827e2c10655656c88
ekramulislam2050 <ekramulislam2050@gmail.com>
2025-05-25 19:17:14 +0600

    added deleted operation

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require("cors")
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware---------
 app.use(cors())
@@ -36,6 +36,7 @@ async function run() {
 
 
         app.get("/menus", async (req, res) => {
+
             const result = await menuCollection.find({}).toArray()
             res.send(result)
         })
@@ -46,10 +47,24 @@ async function run() {
         })
 
         // cart related api-------------------
+           app.get('/carts',async(req,res)=>{
+               const email=req.query.email
+               const query={email:email}
+               const result=await cartCollection.find(query).toArray()
+               res.send(result)
+           })
            app.post("/carts",async(req,res)=>{
               const cartItem = req.body
               const result =await cartCollection.insertOne(cartItem)
               res.send(result)
+           })
+
+
+           app.delete("/carts/:id",async(req,res)=>{
+               const id = req.params.id
+               const query={_id:new ObjectId(id)}
+               const result=await cartCollection.deleteOne(query)
+               res.send(result)
            })
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
