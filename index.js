dd22542a14d76374f7d525a72fa24fdb959054c2
ekramulislam2050 <ekramulislam2050@gmail.com>
2025-06-02 10:02:47 +0600

    added patch method

--- index.js
+++ index.js
@@ -103,6 +103,23 @@ async function run() {
             res.send(result)
         })
 
+        app.patch("/menu/:id", async (req,res)=>{
+                 const item = req.body
+                 const id = req.params.id
+                 const filter={_id:id}
+                 const updatedDoc={
+                    $set:{
+                        name:item.name,
+                        category:item.category,
+                        price:item.price,
+                        recipe:item.recipe,
+                        image:item.image
+                    }
+                 }
+                 const result = await menuCollection.updateOne(filter,updatedDoc)
+                 res.send(result)
+        })
+
         app.get("/reviews", async (req, res) => {
             const result = await reviewCollection.find({}).toArray()
             res.send(result)
